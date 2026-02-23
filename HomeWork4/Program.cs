39d501994bb6a77e16b43338c5366da869ed96d3
Vlad <vladchereshev@gmail.comgit config --global user.name Vladgit statusgit config --global user.email vladchereshev@gmail.com>
2026-02-23 18:15:43 +0100

    homework done

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,7 +1,38 @@
-﻿class Program
+﻿namespace HomeWork4;
+using System.Text;
+
+class Program
 {
     static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
         
+        Warehouse warehouse = new Warehouse();
+        
+        warehouse.OnItemChanged += (index, item) => Console.WriteLine($"У комірку №{index} додано товар {item}");
+        
+        List<string> items = new List<string> { "пляшка", "телефон", "PC" , "шкатулка" , "книга", "", "лампа", "", "", "годинник" };
+        
+        
+        Predicate<string> isValidName = name => !string.IsNullOrWhiteSpace(name);
+        
+        Func<string , string> itemNameToUpper = name => name.ToUpper();
+
+        int addIndex = 0;
+        
+        foreach (string item in items)
+        {
+            if (isValidName(item))
+            {
+                warehouse[addIndex] = itemNameToUpper(item);
+                addIndex++;
+            }
+        }
+
+        for (int i = 0; i < addIndex; i++)
+        {
+            Console.WriteLine($"Комірка №{i}: {warehouse[i]}");
+        }
+        Console.ReadKey();
     }
 }
--- HomeWork4/Warehouse.cs
+++ HomeWork4/Warehouse.cs
@@ -2,7 +2,16 @@
 
 public class Warehouse
 {
-    private string [] _items = new string[10];
-    
-    
+    private string[] _items = new string[10];
+
+    public string this[int index]
+    {
+        get { return _items[index]; }
+        set { _items[index] = value; }
+    }
+
+    public event Action<int, string> OnItemChanged;
 }
+
+    
+        
